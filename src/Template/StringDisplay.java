4cecee6c5f1209daa694dd57cd497604f8b6a069
whitecarrot <whitepanic82@gmail.com>
2016-08-24 23:38:25 +0900

    add template pattern

---
+++ src/Template/StringDisplay.java
@@ -0,0 +1,38 @@
+package Template;
+
+public class StringDisplay extends AbstractDisplay {
+	
+	private String str;
+	private int width;
+	
+	public StringDisplay(String str){
+		this.str = str;
+		this.width = str.getBytes().length;
+	}
+
+	@Override
+	public void open() {
+		printLine();
+		
+	}
+
+	@Override
+	public void print() {
+		System.out.println("|"+str+"|");
+	}
+
+	@Override
+	public void close() {
+		printLine();
+	}
+	
+	private void printLine(){
+		System.out.print("+");
+		for (int i =0; i< this.width; i++){
+			System.out.print("-");
+		}
+		System.out.println("+");
+	}
+	
+
+}
